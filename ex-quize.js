d71de4cce6bab1ddcf1e2b878f07358abc7b7bf3
mizukiwakita <zm18389@josai.ac.jp>
2021-01-22 16:54:35 +0000

    20210123

---
+++ ex-quize.js
@@ -0,0 +1,15 @@
+function nazo(){
+  if (document.getElementById('answer').value == 'ユキ')
+  {alert('正解です！');}
+  else if(document.getElementById('answer').value == 'ユキ')
+    {alert('正解です！')}
+  else {alert('違うよ！');}
+}
+
+function hinto1(){
+    alert('２文字です！')
+}
+
+function hinto2(){
+    alert('初対面は、4月なのに雪が降る日でした')
+}
